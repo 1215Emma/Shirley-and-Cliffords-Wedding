cd668026056e00a18f43b0485b7066836daf4912
Emma Tran <emmayentran@gmail.com>
2021-11-01 20:11:13 -0700

    final hotfix to inner dimension scaling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import MusicPlayerGallery from "./MusicPlayerGallery/MusicPlayerGallery";
 import Rsvp from "./pages/Rsvp/Rsvp";
@@ -9,31 +9,44 @@ import Admin from "./Admin/Admin";
 import Gallery from "./Gallery/Gallery";
 import Registry from "./pages/Registry/Registry";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { useWindowSize } from "./Utilities/useWindowSize";
+// import { debounce } from "./Utilities/useWindowSize";
 
-import Canvas from "./Canvas";
 function App() {
   const [showSidebar, setShowSidebar] = useState(false);
-  const [height, width] = useWindowSize()
-  console.log(width, "width")
-  console.log(height, "height")
-  const resetHeight = () => {
-    // reset the body height to that of the inner browser
-    return (
-      document.body.style.height = window.innerHeight + "px"
-      )
-  }
-  // reset the height whenever the window's resized
-  window.addEventListener("resize", resetHeight);
-  // called to initially set the height.
-  resetHeight();
-  console.log(resetHeight())
+
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth, height: window.innerHeight,
+  });
+  
+  useEffect(() => {
+    const debounce = (fn, delay) => {
+      let timer;
+      return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          fn(...args);
+        }, delay);
+      };
+    };
+    let windowSize = (width, height) => {
+      console.log("width", width)
+      console.log("height", height)
+      windowDimensions.height = height;
+      windowDimensions.width = width;
+    setWindowDimensions({width: width, height: height})
+  };
+  windowSize = debounce(windowSize, 500);
+  window.addEventListener("resize", () =>
+    windowSize(window.innerWidth, window.innerHeight)
+    );
+  }, [windowDimensions])
+
   return (
-    <div className="app" style={{height: `${height}px`, width: `${width}px`}}>
+    <div className="app" style={{height: `${windowDimensions.height}px`, width: `${windowDimensions.width}px`}}>
       <Router>
         <Switch>
           <Route exact path="/">
-            <Home showSidebar={showSidebar} setShowSidebar={setShowSidebar} height={height} width={width} />
+            <Home showSidebar={showSidebar} setShowSidebar={setShowSidebar} height={windowDimensions.height} width={windowDimensions.width} />
           </Route>
           <Route exact path="/admin">
             <Admin showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
--- src/Utilities/useWindowSize.js
+++ src/Utilities/useWindowSize.js
@@ -1,26 +1,22 @@
-import { useState, useLayoutEffect } from 'react'
+import { useState, useEffect, useLayoutEffect } from 'react'
 
+let windowDimensions = {height: window.innerHeight, width: window.innerWidth}
 
-export const useWindowSize = () => {
-  // Initialize state with 0 width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState([window.innerHeight, window.innerWidth]);
-  useLayoutEffect(() => {
-    // Handler to call on window resize
-    const handleResize = () => {
-      // Set window width/height to state
-      setWindowSize([
-        window.innerWidth,
-        window.innerHeight,
-      ]);
-    }
-    // Set size at the first client-side load
-
-    // Add event listener
-    window.addEventListener("resize", handleResize);
- 
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-  return windowSize;
+export const debounce = (fn, delay) => {
+  let timer;
+  return function () {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn()
+    }, delay)
+  }
 }
+
+let windowSize = (height, width) => {
+  windowDimensions.height = height;
+  windowDimensions.width = width;
+}
+
+windowSize = debounce(windowSize, 1000)
+
+window.addEventListener('resize', () => windowSize(window.innerHeight, window.innerWidth))
